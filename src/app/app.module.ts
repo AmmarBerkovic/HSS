f0f3a4f6136c7134f0eb59b23fca9c264cc8c81d
Amina Alibasic <alibasicamina@outlook.com>
2020-12-06 01:39:25 +0100

    Changed background color and added the Logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeTitleComponent } from './welcome-title/welcome-title.component';
 import { DescriptionComponent } from './description/description.component';
-import { PageButtonComponent } from './page-button/page-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeTitleComponent,
     DescriptionComponent,
-    PageButtonComponent,
   ],
   imports: [
     BrowserModule,
