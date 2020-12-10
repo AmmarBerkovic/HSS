93551599110f24e0f6b9b636e04367177ae92c6f
Tarik Muratovic <tarik.muratovic@fet.ba>
2020-12-10 21:05:43 +0100

    Created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFieldsComponent } from './input-fields/input-fields.component';
+import { ButtonComponent } from './button/button.component';
+import { FormFpasswordComponent } from './form-fpassword/form-fpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldsComponent,
+    ButtonComponent,
+    FormFpasswordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-fpassword/form-fpassword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-fpassword',
+  templateUrl: './form-fpassword.component.html',
+  styleUrls: ['./form-fpassword.component.css']
+})
+export class FormFpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
