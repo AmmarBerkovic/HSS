9f3a6e539453f2059958bdd440c74925bb159d59
Amina Alibasic <alibasicamina@outlook.com>
2020-12-11 23:41:52 +0100

    Style & design correction

--- src/webpage-data.ts
+++ src/webpage-data.ts
@@ -3,4 +3,5 @@ export const webpage = {
     description: "This database represents a solution for better organization of stored educational materials. " +
                  "It offers a possibility of searching trough big database of Human Study materials. " +
                  "Advanced search and filtering provides users with additional search or filtering criteria: by title, format, date, etc."
+
 }
