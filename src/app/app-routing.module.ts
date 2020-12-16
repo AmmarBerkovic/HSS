a92af353ae91fd66687233f991165c318ab2e63c
Amina Alibasic <alibasicamina@outlook.com>
2020-12-16 16:24:38 +0100

    Implemented the logic behind the buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, Router } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { FormboxComponent } from './formbox/formbox.component';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registration-form', component: FormboxComponent },
+  { path: 'login-form', component: LoginComponent },
+  { path: 'forgot-password', component: ResetPasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { webpage } from 'src/webpage-data';
 import { Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -16,6 +17,33 @@ export class AppComponent{
       @Input()
       src!: String;
 
-      constructor() { }
+      constructor( private router:Router ) { }
 
+      isHomeRoute(){
+        return this.router.url === '/'; 
+      }
+
+      isRegistrationForm(){
+        return this.router.url === '/registration-form';
+      }
+
+      isLoginForm(){
+        return this.router.url === '/login-form';
+      }
+
+      isForgotPassword(){
+        return this.router.url === '/forgot-password'; 
+      }
+
+      onForgotPasswClicked(){
+        this.router.navigateByUrl('/forgot-password');
+      }
+
+      onSignUpClicked(){
+        this.router.navigateByUrl('/registration-form');
+      }
+
+      onSignInClicked(){
+        this.router.navigateByUrl('/login-form');
+      }
 }
