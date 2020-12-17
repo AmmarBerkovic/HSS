2ac23313a163c7ac0645e2a2c50a5a1c0e996e61
Amina Alibasic <alibasicamina@outlook.com>
2020-12-17 12:35:42 +0100

    Small fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forgot-password', component: ResetPasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
