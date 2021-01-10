dc0b5e2e54e0dc236c853e44bccf25e20e76e9f8
Ammar <ammar.berkovic@gmail.com>
2021-01-10 16:16:20 +0100

    active home, materials and about us layout in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  activeLayout: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setHomeLayout(){
+    this.activeLayout = 1;
+  }
+  setMaterialsLayout(){
+    this.activeLayout = 2;
+  }
+  setAboutUsLayout(){
+    this.activeLayout = 3;
+  }
 }
