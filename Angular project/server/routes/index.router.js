ff8ba1313da7f3d92385967fe17bdc3b02c214ca
jzahirovic <jzahirovic>
2021-01-10 21:33:55 +0100

    Changed design of log out button, edit position

--- Angular project/server/routes/index.router.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-
-
-router.post('/register',ctrlVideo.register);
-router.post('/register',ctrlBook.register);
-
-module.exports=router;
