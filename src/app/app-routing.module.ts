25cf3b5c2efb910c4167a255ea3d437dfb01b127
Amina Alibasic <alibasicamina@outlook.com>
2021-01-13 17:32:04 +0100

    Deleted components from old registration and login forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormboxComponent } from './formbox/formbox.component';
-import { LoginComponent } from './login/login.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
+
 
 
 const routes: Routes = [
-  { path: 'registration-form', component: FormboxComponent },
-  { path: 'forgot-password', component: ResetPasswordComponent },
-  { path: 'login', component: LoginComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeTitleComponent } from './welcome-title/welcome-title.component';
 import { DescriptionComponent } from './description/description.component';
-import { InputfieldsComponent } from './input-fields/input-fields.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { FormboxComponent } from './formbox/formbox.component';
-
-import { LoginComponent } from './login/login.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeTitleComponent,
     DescriptionComponent,
-    FormboxComponent,
-    InputfieldsComponent,
-    ButtonsComponent,
-    FormboxComponent,
-    LoginComponent,
-    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
